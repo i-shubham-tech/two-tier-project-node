6af054b432d615d760acc59a77615e578e94ce68
i-shubham-tech <i-shubham-tech>
2025-10-28 13:45:35 +0530

    feat docker registry and compose file and multistage build

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,20 +4,23 @@ import messageRouter from "./routes/message.route.js"
 
 
 const app = express()
-app.use((req,res,next)=>{
-    console.log(req.method,req.url)
+app.use((req, res, next) => {
+    console.log(req.method, req.url)
     next()
 })
 app.use(express.json())
+app.get("/health", (req, res) => {
+    res.status(200).send("OK");
+});
 app.use("/message", messageRouter)
 app.use((req, res, next) => {
     throw new Error("Not found")
 })
 
 
-app.use((err,req,res,next)=>{
+app.use((err, req, res, next) => {
     console.log(err)
-    res.status(500).json({status:false,message:err.message||"Internal server error"})
+    res.status(500).json({ status: false, message: err.message || "Internal server error" })
 
 })
 
